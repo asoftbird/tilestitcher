973ebc9ac7ba6e89996d94e3756188cb30b6100a
asoftbird <asoftbird@gmail.com>
2022-12-08 10:55:18 +0100

    cleared up some variable names

--- tiler.py
+++ tiler.py
@@ -24,11 +24,11 @@ except:
 
 # defaults
 Image.MAX_IMAGE_PIXELS = 933120000
-input_path = os.getcwd()+"\\input\\"
-output_path = os.getcwd()+"\\output\\output.png"
+INPUT_PATH = os.getcwd()+"\\input\\"
+OUTPUT_PATH = os.getcwd()+"\\output\\output.png"
 GRIDSIZE_X = 0
 GRIDSIZE_Y = 0
-debuglog = False
+DEBUGLOG = False
 
 if not opts:
     sys.exit("Not enough arguments! Use --help for help.")
@@ -39,15 +39,15 @@ for opt, arg in opts:
             print(i)
         sys.exit()
     elif opt in ['-i', '--in']: # input folder
-        input_path = arg
+        INPUT_PATH = arg
     elif opt in ['-o', '--out']: # output file
-        output_path = arg
+        OUTPUT_PATH = arg
     elif opt in ['-x', '--xsize']:
         GRIDSIZE_X = int(arg)
     elif opt in ['-y', '--ysize']:
         GRIDSIZE_Y = int(arg)
     elif opt in ['-v', '--verbose']:
-        debuglog = True
+        DEBUGLOG = True
     else:
         sys.exit("Not enough arguments! Use --help for help.")
 
@@ -55,14 +55,14 @@ if GRIDSIZE_X == 0 or GRIDSIZE_Y == 0:
     sys.exit("Grid size cannot be 0; use --xsize and --ysize to specify tile grid dimensions.")
 
 input_filenames = []
-for file in os.listdir(input_path):
+for file in os.listdir(INPUT_PATH):
     input_filenames.append(file)
 
 
 def merge_images(filelist, gridsize_x, gridsize_y):
     global resulting_width, resulting_height
 
-    image_objects = [Image.open(input_path+filename) for filename in filelist]
+    image_objects = [Image.open(INPUT_PATH+filename) for filename in filelist]
 
     # assuming all images have the same size
     (image_width, image_height) = image_objects[0].size
@@ -73,18 +73,18 @@ def merge_images(filelist, gridsize_x, gridsize_y):
     image_combined = Image.new('RGB', (resulting_width, resulting_height))
 
     image_index = 0
-    for Y_index in range(0, gridsize_y):
-        for X_index in range(0, gridsize_x):
-            if debuglog == True:
-                print(f"X: {X_index} Y: {Y_index}, index: {image_index}, image: {image_objects[X_index]}")
+    for y_index in range(0, gridsize_y):
+        for x_index in range(0, gridsize_x):
+            if DEBUGLOG == True:
+                print(f"X: {x_index} Y: {y_index}, index: {image_index}, image: {image_objects[x_index]}")
             # using an offset height as PIL has the 0,0 coordinate in the upper left corner instead of bottom left
-            image_combined.paste(im=image_objects[image_index], box=(X_index*image_width, resulting_height-(Y_index*image_height)-image_height)) 
+            image_combined.paste(im=image_objects[image_index], box=(x_index*image_width, resulting_height-(y_index*image_height)-image_height)) 
             image_index += 1
 
     return image_combined
 
 merged = merge_images(input_filenames, GRIDSIZE_X, GRIDSIZE_Y) 
 
-merged.save(output_path)
-if os.path.exists(output_path):
-    print(f"Exported image with dimensions {resulting_width}x{resulting_height} ({round(os.stat(output_path).st_size/1E6,2)}MB) to {output_path}.")
+merged.save(OUTPUT_PATH)
+if os.path.exists(OUTPUT_PATH):
+    print(f"Exported image with dimensions {resulting_width}x{resulting_height} ({round(os.stat(OUTPUT_PATH).st_size/1E6,2)}MB) to {OUTPUT_PATH}.")
